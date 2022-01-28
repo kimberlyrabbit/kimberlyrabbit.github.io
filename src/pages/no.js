3c9b7860054c72694cb37df6d9cfac8c3beb9393
Kimmy <karabideau@gmail.com>
2022-01-28 13:02:06 -0500

    no

---
+++ src/pages/no.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import * as React from 'react'
+
+// Step 2: Define your component
+const NoPage = () => {
+  return (
+    <main>
+      <title>No</title>
+      <h1>No</h1>
+      <p>No</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default NoPage
